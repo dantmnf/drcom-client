2accab74d0d7d1512fc7b743f8727b714ae32943
dantmnf <dantmnf2@gmail.com>
2017-09-09 14:00:07 +0800

    use wmic instead of powershell

--- config-windows.php
+++ config-windows.php
@@ -18,7 +18,7 @@ $config->PRIMARY_DNS = "0.0.0.0";
 // use auth server if not set
 $config->dhcp_server = "";
 $config->AUTH_VERSION = "\x2a\x00";
-$config->mac = "\x00\x00\x00\x00\x00\x00";
+$config->mac = hex2bin('114514191981');
 $config->host_os = "2.6.32";
 $config->KEEP_ALIVE_VERSION = "\xd8\x02";
 $config->ror_version = true;
@@ -27,14 +27,43 @@ $config->ror_version = true;
 $config->iface = "";
 
 $config->iface = "Ethernet";
-logger("getting network configuration with powershell");
-exec("powershell -ExecutionPolicy Unrestricted -File " . dirname(__FILE__) . "\getconf.ps1 \"$config->iface\"", $addrs, $exitcode);
 
-if($exitcode === 0) {
-    $config->host_ip = $addrs[0];
-    $config->dhcp_server = $addrs[1];
-    $config->PRIMARY_DNS = $addrs[2];
-} else {
-    logger("failed to get network configuration\n");
+logger("getting network configuration from WMI");
+
+$indexquery = shell_exec('wmic path Win32_NetworkAdapter get Index,NetConnectionID /format:rawxml');
+$sx = simplexml_load_string($indexquery);
+$ncindex = NULL;
+
+foreach($sx->xpath('RESULTS/CIM/INSTANCE') as $inst) {
+    $ncid = $inst->xpath('PROPERTY[@NAME="NetConnectionID"]/VALUE');
+    if(count($ncid) === 0) continue;
+    $ncid = (string)$ncid[0];
+    if($ncid === $config->iface) {
+        $ncindex = (string)$inst->xpath('PROPERTY[@NAME="Index"]/VALUE')[0];
+        break;
+    }
+}
+
+if($ncindex === NULL) {
+    logger("can't find interface {$config->iface}\n");
     exit(1);
 }
+logger('found interface %s with ID %s', $config->iface, $ncindex);
+
+$indexquery = shell_exec('wmic path Win32_NetworkAdapterConfiguration where "Index=' . $ncindex . '" get IPAddress,DHCPServer,DNSServerSearchOrder /format:rawxml');
+$sx = simplexml_load_string($indexquery);
+function str($x) { return (string)$x; }
+$ipaddrs = array_map('str', $sx->xpath('RESULTS/CIM/INSTANCE/PROPERTY.ARRAY[@NAME="IPAddress"]/VALUE.ARRAY/VALUE'));
+$dnsservers = array_map('str', $sx->xpath('RESULTS/CIM/INSTANCE/PROPERTY.ARRAY[@NAME="DNSServerSearchOrder"]/VALUE.ARRAY/VALUE'));
+$fltrv4 = function($x) { return strpos($x, ':') === false; };
+$v4addrs = array_filter($ipaddrs, $fltrv4);
+$v4dnsservers = array_filter($dnsservers, $fltrv4);
+
+if(count($v4addrs)!==1) {
+    logger("can't find unique IPv4 address on interface $config->iface");
+    exit(1);
+}
+$config->host_ip = $v4addrs[0];
+$config->dhcp_server = (string)$sx->xpath('RESULTS/CIM/INSTANCE/PROPERTY[@NAME="DHCPServer"]/VALUE')[0] ? : '0.0.0.0';
+$config->PRIMARY_DNS = count($v4dnsservers) ? $v4dnsservers[0] : '0.0.0.0';
+logger('using host_ip %s, dhcp_server %s, PRIMARY_DNS %s', $config->host_ip, $config->dhcp_server, $config->PRIMARY_DNS);
